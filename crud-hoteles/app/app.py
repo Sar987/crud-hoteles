0e340de577b4d3731888ef1c9399dbdd09234dc8
Sar987 <sarahl.r.c.18@hotmail.com>
2025-06-15 13:33:56 -0400

    Update app.py

--- crud-hoteles/app/app.py
+++ crud-hoteles/app/app.py
@@ -1,21 +1,10 @@
 from flask import Flask
-import os
-import psycopg2
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    try:
-        conn = psycopg2.connect(
-            host=os.environ["DB_HOST"],
-            dbname=os.environ["DB_NAME"],
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"]
-        )
-        return "Conexión exitosa a la base de datos 🟢"
-    except Exception as e:
-        return f"Error de conexión ❌: {e}"
+@app.route('/')
+def inicio():
+    return "¡API de Hoteles funcionando!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
